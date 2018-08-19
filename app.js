9ce8186f094cb4c2965b551e687e67d2e74b4b10
Andres Outon <outon.x@gmail.com>
2018-08-19 16:59:11 -0500

    added the 3 main modules for the application and some eventListeners

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
+// BUDGET CONTROLLER
 var budgetController = (() => {
   // some code
 })();
 
+// UI CONTROLLER
 var UIController = (() => {
   // some code
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = ((budgetCtrl, UICtrl) => {
-  //some code
+  var ctrlAddItem = () => {
+    //1.- Get field input data
+    //
+    //2.- Add item to budget controller
+    //
+    //3.- Add item to the UI
+    //
+    //4.- Calculate the budget
+    //
+    //5.- Display budget on UI
+    console.log("it works!");
+  };
+  // QUERY SELECTORS
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.addEventListener("keypress", e => {
+    if (e.keyCode === 13 || e.which === 13) {
+      ctrlAddItem();
+    }
+  });
 })(budgetController, UIController);
