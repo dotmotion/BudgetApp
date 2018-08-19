c215dd0f8b14eae3f36974d689acbe13a5c5965d
Andres Outon <outon.x@gmail.com>
2018-08-19 17:11:59 -0500

    added code to read the data from the HTML form fields

--- app.js
+++ app.js
@@ -6,13 +6,24 @@ var budgetController = (() => {
 // UI CONTROLLER
 var UIController = (() => {
   // some code
+
+  return {
+    getinput: () => {
+      return {
+        type: document.querySelector(".add__type").value, // 'inc' || 'exp'
+        description: document.querySelector(".add__description").value,
+        value: document.querySelector(".add__value").value
+      };
+    }
+  };
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = ((budgetCtrl, UICtrl) => {
   var ctrlAddItem = () => {
     //1.- Get field input data
-    //
+    var input = UICtrl.getinput();
+    console.log(input);
     //2.- Add item to budget controller
     //
     //3.- Add item to the UI
@@ -20,7 +31,6 @@ var controller = ((budgetCtrl, UICtrl) => {
     //4.- Calculate the budget
     //
     //5.- Display budget on UI
-    console.log("it works!");
   };
   // QUERY SELECTORS
   document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
