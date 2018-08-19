8a66804c36cfbfce9102dbb00e2f9d24cee646fb
Andres Outon <outon.x@gmail.com>
2018-08-19 16:40:36 -0500

    formatted initial code

---
+++ app.js
@@ -0,0 +1,11 @@
+var budgetController = (() => {
+  // some code
+})();
+
+var UIController = (() => {
+  // some code
+})();
+
+var controller = ((budgetCtrl, UICtrl) => {
+  //some code
+})(budgetController, UIController);
